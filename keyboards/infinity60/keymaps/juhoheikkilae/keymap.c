c0c9b280500973fa260b5abb128bf5b135a64dcc
Juho Heikkilä <Juho.Heikkila@rapal.fi>
2019-12-03 07:33:27 +0200

    Started working on LEDs

--- keyboards/infinity60/keymaps/juhoheikkilae/keymap.c
+++ keyboards/infinity60/keymaps/juhoheikkilae/keymap.c
@@ -1,4 +1,7 @@
 #include QMK_KEYBOARD_H
+#include "led_controller.h"
+
+#define _BASE 0
 
 const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Layer 0: Default Layer
@@ -42,9 +45,30 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TRNS,KC_TRNS,KC_TRNS,          KC_TRNS,               KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS),
 };
 
+// uint8_t led_all[63] = {
+//     11,12,13,14,15,16,17,18,21,22,23,24,25,26,27,
+//      28,31,32,33,34,35,36,37,38,41,42,43,44,45,
+//      46,47,48,51,52,53,54,55,56,57,58,61,62,
+//       63,64,65,66,67,68,71,72,73,74,75,76,77,
+//      78,81,82,83,84,86,87
+// };
+
+uint8_t led_all[63] = {
+    11,12,13,14,15,16,17,18,21,22,23,24,25,26,27,
+     28,31,32,33,34,35,36,37,38,41,42,43,44,45,
+     46,47,48,51,52,53,54,55,56,57,58,61,62,
+      63,64,65,66,67,68,71,72,73,74,75,76,77,
+      81,82,83,84,85
+};
+
 // Runs just one time when the keyboard initializes.
 void matrix_init_user(void) {
+    led_controller_init();
 
+    write_led_page(_BASE, led_all, 63);
+    chThdSleepMilliseconds(10);
+
+    chMBPost(&led_mailbox, _BASE, TIME_IMMEDIATE);
 };
 
 // Runs constantly in the background, in a loop.
