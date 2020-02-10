55c6d03009a7d50f39bfce67a7303a410eab43ce
Juho Heikkilä <Juho.Heikkila@rapal.fi>
2020-02-10 08:23:36 +0200

    WIP

--- keyboards/infinity60/keymaps/juhoheikkilae/keymap.c
+++ keyboards/infinity60/keymaps/juhoheikkilae/keymap.c
@@ -26,7 +26,7 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_ESC, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL, KC_BSLS, KC_GRV, \
       KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_BSPC, \
       MT(MOD_LCTL, KC_ESC),KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,KC_ENT,  \
-      KC_LSPO,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSPC,MO(1), \
+      KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,MO(1), \
       COMMENT,  KC_LALT,KC_LGUI,          KC_SPC,                KC_RALT,KC_RGUI,UNCOMMENT, RGUI(KC_L)),
 
     /* Layer 1: HHKB mode (HHKB Fn)
@@ -50,13 +50,13 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TRNS,KC_TRNS,KC_TRNS,          KC_TRNS,               KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS),
 };
 
-// uint8_t led_all[63] = {
-//     11,12,13,14,15,16,17,18,21,22,23,24,25,26,27,
-//      28,31,32,33,34,35,36,37,38,41,42,43,44,45,
-//      46,47,48,51,52,53,54,55,56,57,58,61,62,
-//       63,64,65,66,67,68,71,72,73,74,75,76,77,
-//      78,81,82,83,84,86,87
-// };
+uint8_t led_all[63] = {
+    11,12,13,14,15,16,17,18,21,22,23,24,25,26,27,
+     28,31,32,33,34,35,36,37,38,41,42,43,44,45,
+     46,47,48,51,52,53,54,55,56,57,58,61,62,
+      63,64,65,66,67,68,71,72,73,74,75,76,77,
+     78,81,82,83,84,86,87
+};
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
@@ -72,14 +72,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   }
   return true;
 }
-
-uint8_t led_all[63] = {
-    11,12,13,14,15,16,17,18,21,22,23,24,25,26,27,
-     28,31,32,33,34,35,36,37,38,41,42,43,44,45,
-     46,47,48,51,52,53,54,55,56,57,58,61,62,
-      63,64,65,66,67,68,71,72,73,74,75,76,77,
-      81,82,83,84,85
-};
 
 // Runs just one time when the keyboard initializes.
 void matrix_init_user(void) {
